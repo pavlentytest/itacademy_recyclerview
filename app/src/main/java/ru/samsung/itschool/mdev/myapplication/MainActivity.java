f6c4faeed7bf82b29e7f2d871ed1972d0a5ba075
Pavel <impetus@yandex.ru>
2021-04-13 16:45:43 +0300

    Initial commit

---
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package ru.samsung.itschool.mdev.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String names[] = {
+            "Clouds",
+            "Sun",
+            "Partial clouds",
+            "Snow",
+            "Sleet",
+            "Mist",
+            "Clear",
+            "Rain",
+            "Rain thunder",
+            "Fog"
+    };
+
+    private final String urls[] = {
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/angry_clouds.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/day_clear.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/night_half_moon_partial_cloud.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/night_half_moon_snow.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/sleet.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/mist.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/night_full_moon_clear.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/night_half_moon_rain.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/rain_thunder.png",
+            "https://www.dovora.com/resources/weather-icons/showcase/modern_showcase/fog.png"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.list);
+        // с помощью LayoutManager мы отображаем список - линейно
+        RecyclerView.LayoutManager layoutmanager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutmanager);
+        recyclerView.setAdapter(new MyAdapter());
+    }
+
+    public ArrayList prepareData() {
+        ArrayList<MyImage> result = new ArrayList();
+        for(int i=0;i<names.length;i++) {
+            result.add(new MyImage(names[i],urls[i]));
+        }
+        return result;
+    }
+}
