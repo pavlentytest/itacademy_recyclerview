51776722c506c047f66959ee7b565b428d6e1d45
Pavel <impetus@yandex.ru>
2021-04-13 16:59:53 +0300

    Добавили код в адаптер

--- app/src/main/java/ru/samsung/itschool/mdev/myapplication/MyAdapter.java
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MyAdapter.java
@@ -1,12 +1,25 @@
 package ru.samsung.itschool.mdev.myapplication;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyItem> {
+
+    private Context context;
+    private ArrayList arrayList;
+
+    MyAdapter(ArrayList<MyImage> a, Context c) {
+        this.context = c;
+        this.arrayList = a;
+    }
 
     @NonNull
     @Override
@@ -16,18 +29,24 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyItem> {
 
     @Override
     public void onBindViewHolder(@NonNull MyItem holder, int position) {
-
+        // загрузить картинки
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayList.size();
     }
 
     public class MyItem extends RecyclerView.ViewHolder {
 
+        private ImageView imageView;
+        private TextView textView;
+
         public MyItem(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById();
+            textView = itemView.findViewById();
         }
+
     }
 }
